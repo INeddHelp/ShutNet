29ed70bc8be708d2f8673f377aa969816d6b3e57
INeedHelp <leonardo.bartolelli@gmail.com>
2023-03-09 21:15:43 +0100

    ShutNet v3.1 Powershell script

--- ShutNet.ps1
+++ ShutNet.ps1
@@ -1,10 +1,24 @@
 $ipAddresses = @()
-Get-NetNeighbor -AddressFamily IPv4 | ForEach-Object {
-    if ($_.State -eq "Reachable") {
-        $ipAddresses += $_.IPAddress
+1..254 | ForEach-Object {
+    $ip = "192.168.1.$_"
+    if (Test-Connection -ComputerName $ip -Count 1 -ErrorAction SilentlyContinue) {
+        $ipAddresses += $ip
     }
 }
 
 Set-Content -Path "Ips.txt" -Value $ipAddresses
 
-Stop-Computer -ComputerName $ipAddresses -Force -Confirm:$false
+Param (
+    [Parameter(Mandatory=$true)]
+    [string[]]$IPAddress
+)
+
+foreach ($ip in $IPAddress) {
+    $computerName = (Resolve-DnsName -Name $ip -ErrorAction SilentlyContinue).NameHost
+
+    if ($computerName) {
+        Stop-Computer -ComputerName $computerName -Force -Confirm:$false
+    } else {
+        Write-Warning "Could not resolve computer name for IP address $ip"
+    }
+}
