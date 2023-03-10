6ebb1e436be7be3c036c9af617f0156ea109a350
INeedHelp <leonardo.bartolelli@gmail.com>
2023-03-10 15:29:01 +0100

    Updated version of .ps1

--- ShutNet.ps1
+++ ShutNet.ps1
@@ -1,24 +1,20 @@
 $ipAddresses = @()
-1..254 | ForEach-Object {
-    $ip = "192.168.1.$_"
-    if (Test-Connection -ComputerName $ip -Count 1 -ErrorAction SilentlyContinue) {
+for ($i = 1; $i -le 254; $i++) {
+    $ip = "192.168.1.$i"
+    $pingResult = $(ping -c 1 -W 1 $ip 2>&1)
+    if ($pingResult -match "1 received") {
         $ipAddresses += $ip
     }
 }
 
 Set-Content -Path "Ips.txt" -Value $ipAddresses
 
-Param (
-    [Parameter(Mandatory=$true)]
-    [string[]]$IPAddress
-)
-
-foreach ($ip in $IPAddress) {
-    if (Test-Connection -ComputerName $ip -Count 1 -ErrorAction SilentlyContinue) {
-        $computerName = (Resolve-DnsName -Name $ip -ErrorAction SilentlyContinue).NameHost
-
+foreach ($ip in $ipAddresses) {
+    $pingResult = $(ping -c 1 -W 1 $ip 2>&1)
+    if ($pingResult -match "1 received") {
+        $computerName = $(nslookup $ip | Select-String "name =" | Out-String).Trim() -replace ".*name = ([^\.]*)\..*", '$1'
         if ($computerName) {
-            Stop-Computer -ComputerName $computerName -Force -Confirm:$false
+            $(shutdown -s -f -t 0 -m \\$computerName)
         } else {
             Write-Warning "Could not resolve computer name for IP address $ip"
         }
